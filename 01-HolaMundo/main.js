0fd29356a66277cec8ada363ece464fd02c59e57
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-22 13:55:30 -0600

    condicion if-elseif-else con v-if

--- 01-HolaMundo/main.js
+++ 01-HolaMundo/main.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
                 movies: ["pelicula1", "pelicula2", "pelicula3", "pelicula4", "pelicula5"],
                 urlPinguinos: 'https://cdn.pixabay.com/photo/2018/07/28/02/26/puffins-3567175_960_720.jpg',
                 classValue: "Good",
-                
+
             }            
             
         };
