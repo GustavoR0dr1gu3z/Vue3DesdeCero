3ad13f1b56e42876e3976aa72632ff9c8e4911b3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-22 13:04:25 -0600

    usando vue con html

--- 01-HolaMundo/main.js
+++ 01-HolaMundo/main.js
@@ -3,6 +3,9 @@ const app = Vue.createApp({
     data(){
         return {
             title: 'Hola mundo',
+            name: 'Gustavo Calzada',
+            age: '22',
+            url: 'https://www.google.com.mx/'            
         };
     },
 
