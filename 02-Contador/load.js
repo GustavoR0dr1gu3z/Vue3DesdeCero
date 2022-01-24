b942cb91028d3b5e96577f890da9b37c31eb3077
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-24 14:21:08 -0600

    service worker

--- 02-Contador/load.js
+++ 02-Contador/load.js
@@ -1,6 +1,6 @@
 /*Solo hay un if, que pregunta si hay service worker en el navegador*/
 
-if(serviceWorker in navigator){
+if("serviceWorker" in navigator){
     navigator.serviceWorker.register('./sw.js').then(
         reg => console.log('Registro del service worker', reg),
 
