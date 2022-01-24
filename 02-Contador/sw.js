cf835f2abf1833265074df61e07b240d7df43dd6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-24 14:42:39 -0600

    cache

--- 02-Contador/sw.js
+++ 02-Contador/sw.js
@@ -13,4 +13,35 @@ const urlsToCache = [
     './css/styles.css',
     'https://unpkg.com/vue@next',
     'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
-]
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(
+            cache => cache.addAll(urlsToCache).then(
+                () => self.skipWaiting()
+            ).catch(
+                err => console.log('Error en el registro del cache', err)       
+            )
+        )
+    )
+})
+
+self.addEventListener("activate", e => {
+    const cacheWhiteList = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys().then(
+            cachesNames => {
+                return Promise.all(
+                    cachesNames.map(
+                        cacheName => {
+                            if(cacheWhiteList.indexOf(cacheName) === -1){
+                                return caches.delete(cacheName);
+                            }
+                        }
+                    )
+                )
+            }
+        )
+    )
+})
