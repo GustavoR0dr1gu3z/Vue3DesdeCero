072260c34620e079eb6ec501b78e376d2c69b0e8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-24 14:19:45 -0600

    service worker

--- 02-Contador/load.js
+++ 02-Contador/load.js
@@ -3,6 +3,8 @@
 if(serviceWorker in navigator){
     navigator.serviceWorker.register('./sw.js').then(
         reg => console.log('Registro del service worker', reg),
-        err => console.log('No se pudo registrar el service worker', err)
-    )
+
+    ).catch(
+        err => console.log('Error en el registro del service worker', err)
+    );
 }    
