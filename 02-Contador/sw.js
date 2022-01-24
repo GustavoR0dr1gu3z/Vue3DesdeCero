b6757e2bba2d9334bed46df19306cd1d9030f803
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-24 14:26:43 -0600

    cache

---
+++ 02-Contador/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME ="v1_cache_contador_app_vue";
+const urlsToCache = [
+    './',
+    './img/icono16px.png',
+    './img/icono32px.png',
+    './img/icono64px.png',
+    './img/icono128px.png',
+    './img/icono256px.png',
+    './img/icono512px.png',
+    './img/icono1024px.png',
+]
